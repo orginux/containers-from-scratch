3728db27100bec83361c2b3356c62eeb6e2214d1
orginux <orginux@protonmail.com>
2021-05-30 18:57:29 +0300

    cgoups and check var

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
 func main() {
+	if _, ok := os.LookupEnv("UBUNTUFS_PATH"); !ok {
+		panic("Set env UBUNTUFS_PATH")
+	}
 	switch os.Args[1] {
 	case "run":
 		run()
@@ -26,7 +32,14 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		Credential: &syscall.Credential{Uid: 0, Gid: 0},
+		UidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getuid(), Size: 1},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{ContainerID: 0, HostID: os.Getgid(), Size: 1},
+		},
 	}
 
 	must(cmd.Run())
@@ -34,6 +47,8 @@ func run() {
 
 func child() {
 	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
+
+	cg()
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -44,13 +59,23 @@ func child() {
 	}
 
 	must(syscall.Sethostname([]byte("container")))
-	must(syscall.Chroot(os.Getenv("UBUNTUFS")))
+	must(syscall.Chroot(os.Getenv("UBUNTUFS_PATH")))
 	must(syscall.Chdir("/"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 
 	must(cmd.Run())
 
 	must(syscall.Unmount("proc", 0))
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "den"), 0755)
+	must(ioutil.WriteFile(filepath.Join(pids, "den/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	must(ioutil.WriteFile(filepath.Join(pids, "den/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "den/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
